45b2021649410352ef36accd702afc1fc851fa0c
uminocelo <marcelo.mdl.dev@gmail.com>
2023-02-28 21:39:24 -0300

    generating PDF files with nicoorobin

--- Gemfile
+++ Gemfile
@@ -4,4 +4,5 @@ source "https://rubygems.org"
 
 # gem "rails"
 gem 'rspec', '~> 3.0'
+gem 'pathname', '~> 0.2.1'
 gem 'prawn', '~> 2.4'
--- lib/nicoorobin.rb
+++ lib/nicoorobin.rb
@@ -1,31 +1,50 @@
 require 'pathname'
+require 'prawn'
 
 class Nicoorobin
-    # deve retornar um array de arrays de imagens
-    # e.g: [['path1/1.jpg', 'path1/2.jpg'], [''path2/1.jpg'']]
-    def research(path)
+    attr_accessor :path, :ext_from, :ext_to, :files, :dest
+    
+    def initialize(path, ext_from="jpg", ext_to="pdf", dest="own")
+        @path = path
+        @ext_from = ext_from
+        @ext_to = ext_to
+        @dest = dest == "own" ? @path : dest
+        @filename = File.basename(path)
+    end
+
+    def research
         begin
-            path = Pathname.new(path)
-            raise 'Path is not a directory' unless path.directory?
+            @files = Pathname.glob("#{@path}/*.#{@ext_from}")
+            raise "No file with the #{@ext_from} extension was found" if @files.empty?
 
-            items = path.children
-            return 'No files or folders found!' if items.empty?
-
-            items.map do |item|
-                if item.directory?
-                    "precisa acessar o folder e retornar um array de path das imagens"
-                elsif item.file?
-                    return item.to_path
-                end
+            @files.sort! do |a, b|
+                file_a = Pathname.new(a).basename
+                file_b = Pathname.new(b).basename
+                file_a.sub(".#{@ext_from}", '').to_s.to_i <=> file_b.sub(".#{@ext_from}", '').to_s.to_i
             end
+
+            raise 'Error sorting the files.' if @files.empty?
+
+            @files.map! do |file|
+                file.to_path
+            end
+        rescue Exception => e
+            puts e
+            return nil
         end
     end
 
-    def gather(list_of_files)
-        :not_implemented
-    end
-    
-    def build(list_of_imgs)
-        :not_implemented
+    def build
+        begin
+            raise "No file with the #{@ext_from} extension was found" if @files.empty?
+
+            Prawn::Document.generate("#{@dest}/#{@filename}.#{@ext_to}", page_size: "A4") do |pdf|
+                @files.each do |f|
+                    pdf.image f, scale: 1, position: :center, width: 450
+                end
+            end
+        rescue Exception => e
+            puts e
+        end
     end
 end
